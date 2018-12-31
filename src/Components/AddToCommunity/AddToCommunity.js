de540261335cdf80ffa4836638f8347e3e488f49
SheaParrott <alexandergotthis@gmail.com>
2018-12-30 21:19:57 -0500

    CSS updated for my community component

--- src/Components/AddToCommunity/AddToCommunity.js
+++ src/Components/AddToCommunity/AddToCommunity.js
@@ -4,10 +4,12 @@ import './style.css'
 class AddToCommunity extends Component {
   render() {
     return (
-      <div className="addToCommunity">
-        {/* make clickable */}
-        <i class="fas fa-plus-circle" />
-        <p>Add to Community</p>
+      <div className="addToCommunityBox">
+        <div className="addToCommunity">
+          {/* make clickable */}
+          <i class="fas fa-plus-circle" />
+          <p>Add to Community</p>
+        </div>
       </div>
     )
   }
